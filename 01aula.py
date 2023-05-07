8a1eac6741237e680878188b64270c418fdc8e8e
Gabriel santana leite <111404839+gabesantana@users.noreply.github.com>
2023-05-06 23:32:53 -0300

    Create 01aula.py

---
+++ 01aula.py
@@ -0,0 +1,27 @@
+// comandos em python da aula 1
+
+print('ola mundo')
+
+// se quiser escrever algo na tela utilize aspas 
+// se quiser utilizar os numeros não use aspas.
+
+conta:
+  7+4 
+  caso queira escrever eles na tela use o parenteses
+  
+  print(7+4) - não posso usar aspas já que não é mensagem
+  
+  caso eu use o comando 
+  
+  print('7'+'4') ele vai juntar os números e não somar,
+  no python usar para juntar pode ser o simbolo de "+" ou "," 
+  
+  toda variavel é um objeto no python e para declarar basta usar o sinal "="
+  exemplo:
+    
+    nome='gabriel'
+    idade=22
+    peso=60
+    print(nome,idade,peso)
+    
+    o "+" é usado apenas quando tem uma mensagem + outra mensagem.
